089e3d8ad735fca822a56b866cd286c019ea8cba
Olivier-Masabo <masaboolivier21@gmail.com>
2026-02-11 17:59:39 +0200

    Building first scrimba Project

--- scrimba-project/src/App.jsx
+++ scrimba-project/src/App.jsx
@@ -1,5 +1,5 @@
 
-
+import  ReactFact  from './Components/ReactFacts'
 import './App.css'
 
 function App() {
@@ -7,7 +7,7 @@ function App() {
 
   return (
     <>
-     <h1 className='text-2xl text-green-600 font-bold'>Hello from React</h1>
+     <ReactFact />
     </>
   )
 }
