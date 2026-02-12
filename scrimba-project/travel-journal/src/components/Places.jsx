75eb61b77aa9927378632f4ab4fbf7671943fe9d
Olivier-Masabo <masaboolivier21@gmail.com>
2026-02-12 11:10:55 +0200

    Creating main section of travel journal project

---
+++ scrimba-project/travel-journal/src/components/Places.jsx
@@ -0,0 +1,15 @@
+import image from '../../public/fuji.png'
+function Places(){
+
+
+    return(
+    <>
+    <div>
+    <img src={image} alt="" />
+    
+    </div>
+    </>
+    )
+}
+
+export default Places;
